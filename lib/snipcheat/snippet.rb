0bdcc6731a0dda43a87363d0b2e5fa649b63fd06
Tadas Tamosauskas <tadastamo@gmail.com>
2012-11-28 22:43:34 +0000

    a Snippet class

---
+++ lib/snipcheat/snippet.rb
@@ -0,0 +1,11 @@
+module Snipcheat
+  class Snippet
+    def initialize(path)
+      @path = path
+    end
+
+    def load
+
+    end
+  end
+end
--- lib/snipcheat/snippet_dir.rb
+++ lib/snipcheat/snippet_dir.rb
@@ -1,3 +1,5 @@
+require 'snipcheat/snippet'
+
 module Snipcheat
   class SnippetDir
     def initialize(path)
@@ -6,6 +8,10 @@ module Snipcheat
 
     def empty?
       snippet_filenames.empty?
+    end
+
+    def snippets
+      snippet_filenames.map{ |fn| Snippet.new(File.join(@path, fn)) }
     end
 
     def snippet_filenames
