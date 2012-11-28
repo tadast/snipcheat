ceba1278ffdf5282da38b52a374ec21086a03688
Tadas Tamosauskas <tadas@pdfcv.com>
2012-11-28 18:00:43 +0000

    Find the snippets in the given dir

--- lib/snipcheat/task.rb
+++ lib/snipcheat/task.rb
@@ -2,10 +2,29 @@ module Snipcheat
   class Task < Thor
     include Thor::Actions
 
+    class_option  :verbose, type: :boolean, aliases: '-v'
     method_option :dir, type: :string, aliases: '-d', desc: "Input directory containing .sublime-snippet files", required: true
     method_option :out, type: :string, aliases: '-o', desc: "Output path for the html file"
     desc "generate", "Generate a cheat sheet file given --dir and -out"
     def generate
+      vsay "Looking for snippets in #{options[:dir]}"
+      snippets = Dir.new(options[:dir]).select{ |x|
+        x.end_with? '.sublime-snippet'
+      }.compact
+      if snippets.empty?
+        say "No snippets found in #{options[:dir]}"
+        return
+      else
+        vsay "Found: #{snippets.join(', ')}"
+      end
+    end
+
+  private
+
+    def vsay(message)
+      if options[:verbose]
+        say message
+      end
     end
   end
 end
